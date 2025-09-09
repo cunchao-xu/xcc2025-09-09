880c6ac14ebb05a674990d5d6c9cc6081db71536
xucunchao <1171020333@qq.com>
2025-09-09 15:26:47 +0800

    计算

---
+++ math.java
@@ -0,0 +1,11 @@
+public class math {
+
+    public static void main(String[] args) {
+    }
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+}
