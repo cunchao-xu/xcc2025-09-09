ba867c3cb508e6695eb18026c30abd45e49cc619
xucunchao <1171020333@qq.com>
2025-09-09 15:33:16 +0800

    计算--乘除

--- math.java
+++ math.java
@@ -8,4 +8,11 @@ public class math {
     public static int subtract(int a, int b) {
         return a - b;
     }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+    public static int divide(int a, int b) {
+        return a / b;
+    }
 }
